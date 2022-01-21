36fd8be46df29d3198136e103c1f128521a1603d
Bruno Barbosa <brb.dev@outlook.com>
2022-01-20 20:43:56 -0400

    projeto spring

---
+++ src/test/java/com/ALTbruno/DesafioAPCoders/DesafioApCodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ALTbruno.DesafioAPCoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApCodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
